da041aaac91d947a081cdb0c4e842c3f397086f3
imrainmaker <bazinjeremy@gmail.com>
2023-06-15 15:15:40 +0200

    docker

--- BlazorDemo/Client/Pages/Exercices/Exo2/Exo2.razor.cs
+++ BlazorDemo/Client/Pages/Exercices/Exo2/Exo2.razor.cs
@@ -34,46 +34,25 @@ namespace BlazorDemo.Client.Pages.Exercices.Exo2
 
         private async Task RefreshList()
         {
-            // Logique pour récupérer la liste depuis votre API
             Games = await client.GetFromJsonAsync<List<Game>>("game");
         }
 
-        //protected override async Task OnInitializedAsync()
-        //{
-        //    Games = await client.GetFromJsonAsync<List<Game>>("game");
-        //}
-
-        //public Exo2()
-        //{
-        //    Games = new List<Game>();
-        //    //Games.Add(new Game
-        //    //{
-        //    //    Id = Games.Count + 1,
-        //    //    Title = "Diablo 4",
-        //    //    Genre = "Hack'n slash",
-        //    //    ReleaseYear = 2023,
-        //    //    Note = 2
-        //    //});
-        //}
-
-        //A modifier
         public async Task AddGame(GameForm form)
         {
             Game myGame = new Game
             {
-                //Id = Games.Count + 1,
+
                 Titre = form.Titre,
                 Genre = form.Genre,
                 DateDeSortie = form.DateDeSortie,
                 Note = form.Note
             };
-            //await MyHub.SendAsync("UpdateGameList", myGame);
+
 
             await client.PostAsJsonAsync<Game>("game", myGame);
 
             await RefreshList();
-            //if(t.IsCompleted)
-            //    Games = await client.GetFromJsonAsync<List<Game>>("game");
+
         }
 
         public void SelectGame(int id)
